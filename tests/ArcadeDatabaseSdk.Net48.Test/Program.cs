bb91c9619b819a0477277b516a1537c04fed4e19
motoschifo <michele.fochi@LIVE.IT>
2025-05-21 20:49:30 +0200

    added ADB test project

---
+++ tests/ArcadeDatabaseSdk.Net48.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿#nullable enable
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using ArcadeDatabaseSdk.Net48.Services;
+using static ArcadeDatabaseSdk.Net48.Common.ApiResponse;
+using static ArcadeDatabaseSdk.Net48.Services.Categories.ClassificationsApiResult;
+
+namespace ArcadeDatabaseSdk.Net48.Test;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        // fix async task in net48
+        RunAsync().GetAwaiter().GetResult();
+    }
+
+    static async Task RunAsync()
+    {
+        var adbGenres = await ClassificationsService.Get(classificationType: ClassificationType.Genre, language: LanguageKind.en);
+        Console.WriteLine($"Found {adbGenres.Data.Count} genres");
+        foreach (var genre in adbGenres.Data.OrderBy(x => x.Title))
+        {
+            Console.WriteLine($"- {genre.Title}");
+        }
+
+        var currentLanguage = GetCurrentLanguage();
+        if (currentLanguage != LanguageKind.en)
+        {
+            adbGenres = await ClassificationsService.Get(classificationType: ClassificationType.Genre, language: currentLanguage);
+            Console.WriteLine();
+            Console.WriteLine($"Translated to {currentLanguage}");
+            foreach (var genre in adbGenres.Data.OrderBy(x => x.Title))
+            {
+                Console.WriteLine($"- {genre.Title}");
+            }
+        }
+    }
+}
---
+++ tests/ArcadeDatabaseSdk.Net48.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("ArcadeDatabaseSdk.Net48.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArcadeDatabaseSdk.Net48.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("1c9f749d-e6bb-4bd1-b9fd-c85cef617820")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
